3a301494d52a1161add08c1c0ad9358ab9bf0eb9
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-11-06 09:34:01 -0500

    Adición de las flechas de retorno

--- frontend/src/components/page/ArmSession.tsx
+++ frontend/src/components/page/ArmSession.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
 
 const ArmSession: React.FC = () => {
+    const navigate = useNavigate();
+
     const videoRef = useRef<HTMLVideoElement | null>(null);
 
     useEffect(() => {
@@ -29,21 +33,27 @@ const ArmSession: React.FC = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center p-4 bg-gray-800">
-        <div className="bg-white rounded-lg shadow-lg w-full max-w-4xl overflow-hidden">
-            <video
-            ref={videoRef}
-            className="w-full h-full object-cover"
-            autoPlay
-            playsInline
-            />
-            <div className="p-4">
-            <h1 className="text-3xl font-bold text-center mb-4">Sesión de rehabilitación para brazos</h1>
-            <p className="text-center text-gray-700 mb-4">
-                Realiza ejercicios diseñados para mejorar la fuerza y movilidad de los músculos de tus brazos.
-            </p>
-            {/**/}
+            <button
+                onClick={() => navigate('/rehabilitation')}
+                className="absolute top-4 left-4 text-white hover:text-purple-400 transition-colors"
+            >
+                <ArrowLeft className="h-8 w-8" />
+            </button>
+            <div className="bg-white rounded-lg shadow-lg w-full max-w-4xl overflow-hidden">
+                <video
+                ref={videoRef}
+                className="w-full h-full object-cover"
+                autoPlay
+                playsInline
+                />
+                <div className="p-4">
+                <h1 className="text-3xl font-bold text-center mb-4">Sesión de rehabilitación para brazos</h1>
+                <p className="text-center text-gray-700 mb-4">
+                    Realiza ejercicios diseñados para mejorar la fuerza y movilidad de los músculos de tus brazos.
+                </p>
+                {/**/}
+                </div>
             </div>
-        </div>
         </div>
     );
 };
