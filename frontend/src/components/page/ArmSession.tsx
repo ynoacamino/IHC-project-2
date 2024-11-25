bdb1ce941c641df8ff30c343e5e0136e18b92efb
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-11-25 16:30:42 -0500

    Botones de flechas de retorno

--- frontend/src/components/page/ArmSession.tsx
+++ frontend/src/components/page/ArmSession.tsx
@@ -9,25 +9,25 @@ const ArmSession: React.FC = () => {
 
     useEffect(() => {
         const getCamera = async () => {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-            videoRef.current.play();
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                    videoRef.current.play();
+                }
+            } catch (error) {
+                console.error("Error accessing the camera: ", error);
             }
-        } catch (error) {
-            console.error("Error accessing the camera: ", error);
-        }
         };
 
         getCamera();
 
         return () => {
-        if (videoRef.current && videoRef.current.srcObject) {
-            const stream = videoRef.current.srcObject as MediaStream;
-            const tracks = stream.getTracks();
-            tracks.forEach(track => track.stop());
-        }
+            if (videoRef.current && videoRef.current.srcObject) {
+                const stream = videoRef.current.srcObject as MediaStream;
+                const tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+            }
         };
     }, []);
 
@@ -41,17 +41,16 @@ const ArmSession: React.FC = () => {
             </button>
             <div className="bg-white rounded-lg shadow-lg w-full max-w-4xl overflow-hidden">
                 <video
-                ref={videoRef}
-                className="w-full h-full object-cover"
-                autoPlay
-                playsInline
+                    ref={videoRef}
+                    className="w-full h-full object-cover"
+                    autoPlay
+                    playsInline
                 />
                 <div className="p-4">
-                <h1 className="text-3xl font-bold text-center mb-4">Sesión de rehabilitación para brazos</h1>
-                <p className="text-center text-gray-700 mb-4">
-                    Realiza ejercicios diseñados para mejorar la fuerza y movilidad de los músculos de tus brazos.
-                </p>
-                {/**/}
+                    <h1 className="text-3xl font-bold text-center mb-4">Sesión de rehabilitación para brazos</h1>
+                    <p className="text-center text-gray-700 mb-4">
+                        Realiza ejercicios diseñados para mejorar la fuerza y movilidad de los músculos de tus brazos.
+                    </p>
                 </div>
             </div>
         </div>
