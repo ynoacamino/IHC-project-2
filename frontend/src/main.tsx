f08d1f9b829bc2827284c7866810081ff6430723
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-12-01 15:48:45 -0500

    feat: Envolver SettingsContext con el proveedor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import App from './App';
+import { SettingsProvider } from './components/page/SettingsContext';
 
 const container = document.getElementById('root');
 
@@ -9,6 +10,8 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
   </React.StrictMode>,
 );
