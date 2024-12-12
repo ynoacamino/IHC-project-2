f503b159f63afd791a709b36ff73befba37ebc92
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-12-12 14:01:49 -0500

    Ejercicios

---
+++ frontend/src/components/page/FingerExercise2.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+
+import FingerStep1 from '../../assets/images/finger_ex2_step1.png';
+import FingerStep2 from '../../assets/images/finger_ex2_step2.png';
+
+const FingerExercise2: React.FC = () => {
+    const navigate = useNavigate();
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const [text, setText] = useState('¡Ejercicio 1!');
+    const [showText, setShowText] = useState(true);
+
+    useEffect(() => {
+        const getCamera = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                    videoRef.current.play();
+                }
+            } catch (error) {
+                console.error("Error accessing the camera: ", error);
+            }
+        };
+
+        getCamera();
+
+        return () => {
+            if (videoRef.current && videoRef.current.srcObject) {
+                const stream = videoRef.current.srcObject as MediaStream;
+                const tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        const timer1 = setTimeout(() => {
+            setShowText(false);
+            setTimeout(() => {
+                setText("Sigue los pasos ubicados en la zona izquierda de la pantalla");
+                setShowText(true);
+            }, 500);
+        }, 3000);
+
+        const timer2 = setTimeout(() => {
+            setShowText(false);
+            setTimeout(() => {
+                setText("");
+            }, 500);
+        }, 6000);
+    
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+        };
+    }, []);
+
+    return (
+        <div className="min-h-screen flex items-center justify-center p-4 bg-gradient-to-br from-gray-900 via-blue-900 to-indigo-900">
+            <button
+                onClick={() => navigate('/finger-session')}
+                className="absolute top-4 left-4 text-white hover:text-blue-400 transition-colors"
+            >
+                <ArrowLeft className="h-8 w-8" />
+            </button>
+            <h1 className="absolute top-20 left-1/2 transform -translate-x-1/2 text-5xl font-bold text-white text-center mb-4 z-10">
+                Segundo Ejercicio de Dedos
+            </h1>
+            <div className="flex w-full max-w-6xl items-center space-x-20">
+                {/* Tutorial */}
+                <div className="bg-gray-700 w-1/4 h-[27rem] rounded-lg shadow-lg p-6 flex flex-col">
+                    <h2 className="text-gray-100 font-bold text-3xl text-center mb-5">P A S O S</h2>
+                    <div className="grid grid-rows-2 gap-5 flex-grow">
+                        <div className="bg-gray-700 rounded-lg overflow-hidden flex items-center justify-center text-white text-center font-bold text-lg transition-all cursor-pointer hover:brightness-75 hover:scale-95 transform duration-300">
+                            <img src={FingerStep1} alt="Paso 1" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="bg-gray-700 rounded-lg overflow-hidden flex items-center justify-center text-white text-center font-bold text-lg transition-all cursor-pointer hover:brightness-75 hover:scale-95 transform duration-300">
+                            <img src={FingerStep2} alt="Paso 2" className="w-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Contenido de la cámara */}
+                <div className="relative bg-white rounded-lg shadow-lg w-3/4 overflow-hidden">
+                    <video
+                        ref={videoRef}
+                        className="w-full h-full object-cover"
+                        style={{ transform: 'scaleX(-1)' }}
+                        autoPlay
+                        playsInline
+                    />
+                    {text && (
+                        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-50 p-4">
+                            <h1
+                                className={`text-5xl font-bold text-white text-center mb-4 transition-opacity duration-500 ${
+                                    showText ? 'opacity-100' : 'opacity-0'
+                                }`}
+                            >
+                                {text}
+                            </h1>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/* Botones para Ejercicio */}
+            <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 flex items-center space-x-4">
+                {/* Botón Ejercicio 1 */}
+                <button 
+                    onClick={() => navigate('/finger-exercise-1')}
+                    className="px-9 py-5 rounded-full bg-gray-600 text-white font-semibold text-2xl hover:bg-gray-700 transition-colors"
+                >
+                    Ejercicio 1
+                </button>
+
+                {/* Circulitos de separación */}
+                <div className="flex items-center space-x-2">
+                    <div className="w-4 h-4 rounded-full bg-white"></div>
+                    <div className="w-4 h-4 rounded-full bg-white"></div>
+                    <div className="w-4 h-4 rounded-full bg-white"></div>
+                    <div className="w-4 h-4 rounded-full bg-white"></div>
+                </div>
+
+                {/* Botón Ejercicio 2 */}
+                <button 
+                    onClick={() => navigate('/finger-exercise-2')}
+                    className="px-9 py-5 rounded-full bg-green-600 text-white font-semibold text-2xl hover:bg-green-700 transition-colors"
+                >
+                    Ejercicio 2
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default FingerExercise2;
